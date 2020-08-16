dc5c12d0397c2f282b3d338115ca5bf679e3b9c6
oksana-1 <4438488@gmail.com>
2020-08-16 18:06:41 +0300

    Eject interceptors on componentDidUnmount

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
-  render() {
+    render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder/>
+            <BurgerBuilder/>
         </Layout>
       </div>
     );
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -7,15 +7,19 @@ const withErrorHandler = (WrappedComponent, axios) => {
             error: null
         }
         componentWillMount() {
-            axios.interceptors.request.use(request => {
+            this.requestInterceptor = axios.interceptors.request.use(request => {
                 this.setState({error: null});
                 return request;
             }) ;
-            axios.interceptors.response.use(
+            this.responseInterceptor = axios.interceptors.response.use(
             response => response,
             error => {
                 this.setState({error: error});
             });
+        }
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.response.eject(this.responseInterceptor);
         }
         errorConfirmedHandler = () => {
             this.setState({error: null});
